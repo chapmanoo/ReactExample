c724df519e0f30023b52e79a800db34be506cfe9
chapmanoo <chapmanoo@hotmail.co.uk>
2018-01-03 13:57:37 +0000

    Hardcoded data being shown on front-end

--- exampleapp/src/App.js
+++ exampleapp/src/App.js
@@ -3,6 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      films: [
+        {
+          id: '1',
+          title: 'Toy Story',
+          runTime: 81,
+          release: 1995
+        }, {
+          id: '2',
+          title: 'test',
+          runTime: 121,
+          release: 1994
+        }, {
+          id: '3',
+          title: 'hope',
+          runTime: 96,
+          release: 2003
+        }
+      ],
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -13,6 +38,10 @@ class App extends Component {
         <p className="App-intro">
           Hello there my friend <code>src/App.js</code> and save to reload.
         </p>
+        {
+          this.state.films.map((film) => (
+            <p key={film.id.toString()}> Film title: {film.title}, Film runtime: {film.runTime}, Film release: {film.release} </p>))
+        }
       </div>
     );
   }
